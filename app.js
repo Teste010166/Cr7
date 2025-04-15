f5fb3cce6cb44b43e4ce303d48fbedbb83774ce3
Teste010166 <tubepure2@gmail.com>
2025-04-15 18:01:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const app = document.getElementById('app');
+app.innerHTML = `
+  <h1>Plano CR7</h1>
+  <ul>
+    <li><input type='checkbox'/> Treino físico</li>
+    <li><input type='checkbox'/> Comunicação</li>
+    <li><input type='checkbox'/> Leitura</li>
+    <li><input type='checkbox'/> Projeto Free Fire</li>
+    <li><input type='checkbox'/> Evolução pessoal</li>
+  </ul>
+  <p style='margin-top:20px;'>Marque tudo que cumprir hoje. Mentalidade de campeão!</p>
+`;
